242e35fdf007f94d708a0edebd3fe10b9f0e9c22
Forgit1212 <forgit@list.ru>
2021-12-16 08:11:13 +0300

    16.12

--- src/main/java/com/geekbrains/lesson3dz/Excep.java
+++ src/main/java/com/geekbrains/lesson3dz/Excep.java
@@ -11,7 +11,18 @@ public class Excep {
     }
 }
 
-class MyArraySizeException extends ArrayIndexOutOfBoundsException{
+public class MyArraySizeException extends ArrayIndexOutOfBoundsException{
+    private int row;
+    private int column;
+    private Object[][] array = new Object[column][row];
+
+    public MyArraySizeException (int row, int column, Object value){
+        super ("Invalid massiv size. Length "+array.length);
+        Object[][] array = new Object[column][row];
+        this.row = row;
+        this.column = column;
+        this.value = array[column][row];
+    }
     
 }
 
